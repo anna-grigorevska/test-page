c1fb79b8316b7e94fe5cefca4314cfaa606e9261
Vladislav Lypivets <avasl333@gmail.com>
2020-11-12 23:43:11 +0200

    calendar finish

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
+  // nice-select init
   $('.nice-select').niceSelect();
+
+  // print button handler
   $('#print').click(function(){
     window.print();
     return;
   });
 
-
-  function datePickerSelect(date) {
-    const selectedElement = $('.dp_selected')[0];
-    if ($('.dp_selected .date-picker-tooltip').length === 0) {
-      $('#tooltip').clone().removeAttr('id').appendTo(selectedElement);
-    }
-  }
-
-
+  // date-picker init
   $('.date-picker').Zebra_DatePicker({
     always_visible: $('#date-picker-container'),
     days: ['НД', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ' ],
@@ -21,25 +16,39 @@ $(document).ready(function() {
     navigation: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
     disabled_dates: ['29 10 2020', '30 10 2020', '31 10 2020', '1 11 2020', '2 11 2020', '3 11 2020', '4 11 2020'],
     select_other_months: true,
-    onSelect: datePickerSelect,
+    onChange: initDateTooltip,
+    onSelect: openModal,
   });
 
+  // add listener for days
+  function initDateTooltip() {
+    $('.dp_daypicker.dp_body td').mouseover(function() {
+      if ($(this).find('.date-picker-tooltip').length === 0) {
+        $('#tooltip').clone().removeAttr('id').appendTo(this);
+      }
+    });
+  }
 
+  // openModal
+  function openModal() {
+    $("#calendarModal").modal()
+  }
+
+  // mobile navigation
   $('.burger').click(function(){
     $(this).toggleClass('open');
     $('.menu').toggleClass('open');
-  })
+  });
   $('.burger').click(function(){
     $(this).toggleClass('open');
     $('.overlley').toggleClass('d-block');
-  })
+  });
 
+  // show/hide icons
   $('.btn-link').click(function(){
     $('.plus').toggleClass('d-none');
-    
   })
   $('.btn-link').click(function(){
     $('.icon-block .done').toggleClass('d-block');
-    
-  })
+  });
 });
