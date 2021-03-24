13c6e62da2e21c378170204e631dc9a2a51081b7
anna-grigorevska <anna1997grigorewska@gmail.com>
2021-03-24 14:45:08 +0200

    first commit

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,41 +1,7 @@
 $(document).ready(function() {
-  // nice-select init
-  $('.nice-select').niceSelect();
-
-  // print button handler
-  $('#print').click(function(){
-    window.print();
-    return;
-  });
-
-  // date-picker init
-  $('.date-picker').Zebra_DatePicker({
-    always_visible: $('#date-picker-container'),
-    days: ['НД', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ' ],
-    months: ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень', ],
-    navigation: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-    disabled_dates: ['29 10 2020', '30 10 2020', '31 10 2020', '1 11 2020', '2 11 2020', '3 11 2020', '4 11 2020'],
-    select_other_months: true,
-    onChange: initDateTooltip,
-    onSelect: openModal,
-  });
-
-  // add listener for days
-  function initDateTooltip() {
-    $('.dp_daypicker.dp_body td').mouseover(function() {
-      if ($(this).find('.date-picker-tooltip').length === 0) {
-        $('#tooltip').clone().removeAttr('id').appendTo(this);
-      }
-    });
-  }
-
-  // openModal
-  function openModal() {
-    $("#calendarModal").modal();
-  }
 
   // mobile navigation
-  $('.burger').click(function(){
+  $('.btn-menu').click(function(){
     $(this).toggleClass('open');
     $('.menu').toggleClass('open');
   });
@@ -44,25 +10,19 @@ $(document).ready(function() {
     $('.overlley').toggleClass('d-block');
   });
 
-  // project cost show/hide icons
-  $('.project-cost-main .btn-link').click(function(){
-    $(this).find('.plus').toggleClass('d-none');
-    $(this).find('.icon-block .done').toggleClass('d-block');
+  $('#slick-carousel').slick({
+    slidesToShow: 7,
+    vertical: true,
+    infinite: true,
+    prevArrow: '<button class="btn-prev"><img src="/img/up.svg"></button>',
+    nextArrow: '<button class="btn-next"><img src="/img/down.svg"></button>',
+    responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+    ]
   });
-
-  // wow init
-  new WOW().init();
-
-  // project-cost more button handler
-  $('button.more').click(function(){
-    $(this).toggleClass('rotate');
-    $(this).find('span').toggleClass('d-none');
-    $('.table .hidden').toggleClass('open');
-  });
-
-  // project-cost additional functionality
-  $('#additional-functionality-add').click(function(){
-    $('.additional-functionality').removeClass('d-none');
-    $('#additionalFunctional').modal('toggle');
-  });
-});
+})
--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,29 +1,13 @@
 // Импортируем jQuery
 //= ../../../node_modules/jquery/dist/jquery.js
 
-// Импортируем Popper
-//= ../../../node_modules/popper.js/dist/umd/popper.js
-
-// Nice select
-//= ../../../node_modules/jquery-nice-select/js/jquery.nice-select.min.js
-
 // Импортируем необходимые js-файлы Bootstrap 4
 //= ../../../node_modules/bootstrap/js/dist/util.js
-//= ../../../node_modules/bootstrap/js/dist/alert.js
-//= ../../../node_modules/bootstrap/js/dist/button.js
-//= ../../../node_modules/bootstrap/js/dist/carousel.js
 //= ../../../node_modules/bootstrap/js/dist/collapse.js
-//= ../../../node_modules/bootstrap/js/dist/dropdown.js
-//= ../../../node_modules/bootstrap/js/dist/modal.js
-//= ../../../node_modules/bootstrap/js/dist/tooltip.js
-//= ../../../node_modules/bootstrap/js/dist/popover.js
-//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
 //= ../../../node_modules/bootstrap/js/dist/tab.js
-//= ../../../node_modules/bootstrap/js/dist/toast.js
 
 // Modules
-//= ../../../node_modules/zebra_datepicker/dist/zebra_datepicker.min.js
-//= ../../../node_modules/wowjs/dist/wow.min.js
+//= ../../../node_modules/slick-carousel/slick/slick.min.js
 
 // Импортируем другие js-файлы
 //= ./index.js
